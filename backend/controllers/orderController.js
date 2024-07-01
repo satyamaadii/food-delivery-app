46943bf699c1da0601b946395c75c67296d2e8d3
satyamaadii <153155686+satyamaadii@users.noreply.github.com>
2024-07-01 10:35:20 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //config variables
 const currency = "usd";
 const deliveryCharge = 50;
-const frontend_URL = 'http://localhost:5173';
+const frontend_URL = 'https://food-delivery-frontend-yzf6.onrender.com/';
 
 // Placing User Order for Frontend using stripe
 const placeOrder = async (req, res) => {
